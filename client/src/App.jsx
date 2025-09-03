2cb04bc341f1e0b268062a98222bcc06c463a88c
shashi prakash singh <singhshashi09871@gmail.com>
2025-09-03 19:02:00 +0530

    cleaned up server.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Chat from './Chat.jsx'
 const App = () => {
   return (
--- server.js
+++ server.js
@@ -1181,42 +1181,42 @@
 // })
 
 
-//web socket using react+express
+web socket using react+express
 
-// const express=require("express")
-// const http=require("http")
-// const {Server}=require("socket.io")
-// const cors=require("cors")
-// const app=express()
-// app.use(cors({
-//   origin:"http://localhost:5175",
-//   methods:["GET","POST"],
-//   credentials:true
-// }));
-// //creating a http server
-// const server=http.createServer(app);
-// //creating a new web socket server which connects to the http server
-// const io=new Server(server,{
-//   cors:{origin:"http://localhost:5175"},
-//   methods:["GET","POST"],
-//   credentials:true
-// });
-// //setting up connection
-// io.on("connection",(socket)=>{
-// console.log(`user connected wit id ${socket.id}`);
+const express=require("express")
+const http=require("http")
+const {Server}=require("socket.io")
+const cors=require("cors")
+const app=express()
+app.use(cors({
+  origin:"http://localhost:5175",
+  methods:["GET","POST"],
+  credentials:true
+}));
+//creating a http server
+const server=http.createServer(app);
+//creating a new web socket server which connects to the http server
+const io=new Server(server,{
+  cors:{origin:"http://localhost:5175"},
+  methods:["GET","POST"],
+  credentials:true
+});
+//setting up connection
+io.on("connection",(socket)=>{
+console.log(`user connected wit id ${socket.id}`);
 
-// socket.on("chatMessage",(message)=>{
-//         console.log(`user message is ${message}`);
-//         //broadcast to the all clients
-//         io.emit("chatMessage",message);
-// });
-// socket.on("disconnect",()=>{
-//   console.log(`user disconnected with id ${socket.id}`);
-// });
-// });
-// server.listen(8080,()=>{
-//   console.log("server is running on port 8080")
-// });
+socket.on("chatMessage",(message)=>{
+        console.log(`user message is ${message}`);
+        //broadcast to the all clients
+        io.emit("chatMessage",message);
+});
+socket.on("disconnect",()=>{
+  console.log(`user disconnected with id ${socket.id}`);
+});
+});
+server.listen(8080,()=>{
+  console.log("server is running on port 8080")
+});
 
 
 
